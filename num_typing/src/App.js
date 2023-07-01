30fe8ba9e8afc8f5867d1217c358d9239eae4346
DCKakeru <kakeru.fukuda.0228@gmail.com>
2023-07-01 23:37:53 +0900

    問題を生成できるように修正

--- num_typing/src/App.js
+++ num_typing/src/App.js
@@ -6,30 +6,83 @@ import Status from "./components/modules/Status";
 import { TextField } from "@mui/material";
 
 export default function App() {
+  const generateQuestion = (digit) => {
+    return (
+      Math.floor(Math.random() * (10 ** digit - 10 ** (digit - 1))) +
+      10 ** (digit - 1)
+    ).toString();
+  };
+
   const [question, setQuestion] = useState({
-    name: "456",
+    digit: 4,
+    name: generateQuestion(4),
     state: false,
     answer: "",
   });
-  const handleChangedAnswer = (e) => {
-    const nowAnswer = e;
+
+  const handleChangedAnswer = (targetValue) => {
+    const nowAnswer = targetValue;
     if (nowAnswer === question.name) {
       console.log("正解");
+      collectAnswer();
+    } else {
+      setQuestion((props) => {
+        return {
+          digit: props.digit,
+          name: props.name,
+          state: nowAnswer === question.name,
+          answer: nowAnswer,
+        };
+      });
     }
+  };
+
+  const collectAnswer = () => {
+    console.log("正解後の処理");
+    const name = generateQuestion(question.digit);
     setQuestion((props) => {
       return {
-        name: props.name,
-        state: nowAnswer === question.name,
-        answer: nowAnswer,
+        digit: props.digit,
+        name: name.toString(),
+        state: true,
+        answer: "",
       };
     });
   };
+
+  const handleChangedDigit = (digitValue) => {
+    setQuestion((props) => {
+      return {
+        digit: Number(digitValue),
+        name: generateQuestion(Number(digitValue)).toString(),
+        state: false,
+        answer: "",
+      };
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
       <main>
+        <div>
+          <TextField
+            id="standard-basic"
+            label="桁数"
+            margin="normal"
+            variant="filled"
+            type="number"
+            value={question.digit}
+            onChange={(e) => handleChangedDigit(e.currentTarget.value)}
+            InputProps={{
+              style: { fontSize: 50, height: 150 },
+            }}
+            InputLabelProps={{ style: { fontSize: 40 }, shrink: true }}
+            sx={{ display: "flex" }}
+          />
+        </div>
         <div>
           <Question name={question.name} />
         </div>
