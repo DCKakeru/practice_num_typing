1229f09f2df4ad396c84ded09048941ddf128429
DCKakeru <kakeru.fukuda.0228@gmail.com>
2023-11-05 13:19:40 +0900

    ビルドをdocsにup

--- num_typing/src/components/modules/JudgeAnswer.js
+++ num_typing/src/components/modules/JudgeAnswer.js
@@ -1,41 +1,42 @@
-import React from "react";
-import { TextField } from "@mui/material";
+// 未使用
+// import React from "react";
+// import { TextField } from "@mui/material";
 
-export default class AnswerState extends React.Component {
-  constructor(props) {
-    super(props);
-    this.value = { s: "" };
-    this.onChange = this.onChange.bind(this);
-  }
+// export default class AnswerState extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.value = { s: "" };
+//     this.onChange = this.onChange.bind(this);
+//   }
 
-  onChange(e) {
-    this.setState({ s: e.target.value });
+//   onChange(e) {
+//     this.setState({ s: e.target.value });
 
-    if (this.props.onAnswerChanged) {
-      this.props.onAnswerChanged(e.target.value);
-    }
-  }
+//     if (this.props.onAnswerChanged) {
+//       this.props.onAnswerChanged(e.target.value);
+//     }
+//   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return true;
-  }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     return true;
+//   }
 
-  render() {
-    return (
-      <TextField
-        id="standard-basic"
-        label="回答欄"
-        margin="normal"
-        variant="filled"
-        value={this.value.s}
-        onChange={this.onChange}
-        InputProps={{
-          style: { fontSize: 60, height: 200 },
-        }}
-        InputLabelProps={{ style: { fontSize: 40 }, shrink: true }}
-        sx={{ display: "flex" }}
-        fullWidth
-      />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <TextField
+//         id="standard-basic"
+//         label="回答欄"
+//         margin="normal"
+//         variant="filled"
+//         value={this.value.s}
+//         onChange={this.onChange}
+//         InputProps={{
+//           style: { fontSize: 60, height: 200 },
+//         }}
+//         InputLabelProps={{ style: { fontSize: 40 }, shrink: true }}
+//         sx={{ display: "flex" }}
+//         fullWidth
+//       />
+//     );
+//   }
+// }
